cb079edc3a24b0fadbf85330a2952eca9ede6452
Nayan.Dahare <nayandahare02@gmail.com>
2025-12-18 23:29:42 +0530

    first commit

---
+++ Data Structure & Algorithms/Day 1/intro.java
@@ -0,0 +1,23 @@
+import java.utils.*;
+
+public class intro{
+    public static void main(String[] args) {
+     
+        int[] arr = {5, 7, 1, 21, 25};
+
+        //get
+        System.out.println(arr[2]);
+
+        //set
+        arr[2] = 30;
+
+        //length
+        sout arr.length;
+
+        //traverse
+        for(int i = 0 ; i<arr.length; i++)
+        {
+            //arr[i]
+        }
+    }
+}

f1aaeae43d60b04abb5e5b06047d75d8bab85b3a
Nayan.Dahare <nayandahare02@gmail.com>
2025-12-19 19:29:25 +0530

    DAY 2 commit

---
+++ Data Structure & Algorithms/Day 2/MaxArr.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class MaxArr {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        //Main logic
+        int max = Integer.MIN_VALUE;
+
+        for(int i = 0 ; i < arr.length-1 ; i++){
+            if(arr[i] > max){
+                max = arr[i];
+            }
+        }
+        System.out.println(max);
+
+    }
+}

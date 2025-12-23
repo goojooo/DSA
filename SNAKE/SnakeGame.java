8d378d9561fdf06d62da531b33c47bbabd5720aa
Nayan.Dahare <nayandahare02@gmail.com>
2025-12-23 16:03:35 +0530

    SNAKE game 25.29 commit

--- SNAKE/SnakeGame.java
+++ SNAKE/SnakeGame.java
@@ -4,46 +4,103 @@ import java.util.ArrayList;
 import java.util.Random;
 import javax.swing.*;
 
-public class SnakeGame extends JPanel{
-    private class Tile{
+public class SnakeGame extends JPanel implements ActionListener, KeyListener {
+    private class Tile {
         int x;
         int y;
 
-        Tile(int x, int y){
+        Tile(int x, int y) {
             this.x = x;
             this.y = y;
         }
     }
+
     int borderWidth;
     int borderHeight;
     int tileSize = 25;
 
+    // Snake
     Tile snakeHead;
 
-    SnakeGame(int borderWidth, int borderHeight){
+    // Food
+    Tile food;
+    Random random;
+
+    // Game Logic
+    Timer gameLoop;
+    int velocityX;
+    int velocityY;
+
+    SnakeGame(int borderWidth, int borderHeight) {
         this.borderWidth = borderWidth;
         this.borderHeight = borderHeight;
         setPreferredSize(new Dimension(this.borderWidth, this.borderHeight));
         setBackground(Color.black);
 
-        snakeHead = new Tile(5 , 5);
+        snakeHead = new Tile(5, 5);
+
+        food = new Tile(10, 10);
+        random = new Random();
+        placeFood();
+
+        velocityX = 0;
+        velocityY = 0;
+
+        gameLoop = new Timer(100, this);
+        gameLoop.start();
     }
 
-    public void paintComponent(Graphics g){
+    public void paintComponent(Graphics g) {
         super.paintComponent(g);
         draw(g);
     }
 
-    public void draw(Graphics g){
+    public void draw(Graphics g) {
         // Grid
-        for(int i = 0 ; i < borderWidth/tileSize; i++){
+        for (int i = 0; i < borderWidth / tileSize; i++) {
             // (x1,y1,x2,y2)
-            g.drawLine(i*tileSize, 0, i*tileSize, borderHeight);
-            g.drawLine(0, i*tileSize,borderWidth, i*tileSize);
+            g.drawLine(i * tileSize, 0, i * tileSize, borderHeight);
+            g.drawLine(0, i * tileSize, borderWidth, i * tileSize);
         }
+
+        // Food
+        g.setColor(Color.red);
+        g.fillRect(food.x * tileSize, food.y * tileSize, tileSize, tileSize);
 
         // Snake
         g.setColor(Color.green);
         g.fillRect(snakeHead.x * tileSize, snakeHead.y * tileSize, tileSize, tileSize);
     }
+
+    public void placeFood() {
+        food.x = random.nextInt(borderWidth / tileSize); // 600/25 = 24
+        food.y = random.nextInt(borderHeight / tileSize); //
+    }
+
+    public void move() {
+        // Snake head
+        snakeHead.x += velocityX;
+        snakeHead.y += velocityY;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        move();
+        repaint();
+
+        // throw new UnsupportedOperationException("Unimplemented method...");
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'keyPressed'");
+    }
+
+    // Not needed
+    @Override
+    public void keyTyped(KeyEvent e) {}
+
+    @Override
+    public void keyReleased(KeyEvent e) {}
 }
